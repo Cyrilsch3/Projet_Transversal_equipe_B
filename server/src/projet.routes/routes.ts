fdfbe04ba2db6de20ddd990bc1acb53e75e126b7
Rayan K <r.kinet@students.ephec.be>
2026-05-11 15:53:03 +0200

    ajout #5

--- server/src/projet.routes/routes.ts
+++ server/src/projet.routes/routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { projetController } from '../projet.controller/projet.controller';
+import { projetController } from '../projet.controller/projet.controller.ts';
 const router = Router();
 
 router.post('/assign-card', projetController.assignCard);
 router.get('/users', projetController.getAllUsers);
 router.delete('/users/:id', projetController.deleteUser);
 router.get('/present', projetController.getPresent);      
-
+router.get('/rfid',projetController.scanBadge )
 export default router;
