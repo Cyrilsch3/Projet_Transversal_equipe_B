7e6c1e2b5fffd6c7adbb58cd1124a5ad66dd166b
L <louis@pro-roger.com>
2026-05-13 09:23:45 +0200

    ajout

--- server/src/config/broker.ts
+++ server/src/config/broker.ts
@@ -1,13 +1,23 @@
 import { Aedes } from 'aedes';
 import { createServer } from 'net';
+import { createServer as createHttpServer } from 'http';
+import ws from 'websocket-stream';
 
 const MQTT_PORT = 1883;
+const WS_PORT = 9001;
 
 Aedes.createBroker().then((broker) => {
+    // Serveur TCP pour le backend et la partie elec
     const server = createServer(broker.handle.bind(broker));
-
     server.listen(MQTT_PORT, () => {
         console.log(`🚀 Broker MQTT démarré sur le port ${MQTT_PORT}`);
+    });
+
+    // Serveur WebSocket pour le frontend (navigateur)
+    const httpServer = createHttpServer();
+    ws.createServer({ server: httpServer }, broker.handle.bind(broker));
+    httpServer.listen(WS_PORT, () => {
+        console.log(`🌐 Broker MQTT WebSocket démarré sur le port ${WS_PORT}`);
     });
 
     broker.on('client', (client) => {
---
+++ server/src/projet.modele/projet.logs.ts
@@ -0,0 +1,23 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../config/db.ts';
+
+class Log extends Model {
+    declare id: number;
+    declare username: string;
+    declare id_carte: string;
+    declare action: 'entree' | 'sortie';
+    declare timestamp: Date;
+}
+
+Log.init({
+    username: { type: DataTypes.STRING, allowNull: false },
+    id_carte: { type: DataTypes.STRING, allowNull: false },
+    action:   { type: DataTypes.ENUM('entree', 'sortie'), allowNull: false },
+    timestamp:{ type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
+}, {
+    sequelize,
+    modelName: 'Log',
+    timestamps: false,
+});
+
+export default Log;
